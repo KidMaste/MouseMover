b2a6cbc914bc5544f55807fc01c0562ee53a40a6
Matteo <matte01@outlook.it>
2023-08-25 10:25:25 +0200

    Codice identico a Power Automate, forse

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use mouse_rs::{Mouse};
+use std::{thread, time};
+
+fn main() {
+    let mouse = Mouse::new();
+    loop {
+        let mut position = mouse.get_position().expect("Unable to retrieve mouse position");
+        mouse.move_to(position.x+1, position.y+1).expect("Unable to move mouse");
+        thread::sleep(time::Duration::from_millis(500));
+        position = mouse.get_position().expect("Unable to retrieve mouse position");
+        mouse.move_to(position.x-1, position.y-1).expect("Unable to move mouse");
+        thread::sleep(time::Duration::from_millis(5000));
+    }
+}
