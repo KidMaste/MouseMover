a691c8df9ba0a03d44543eec1521380dc538dc1b
Matteo <matte01@outlook.it>
2023-09-11 10:25:32 +0200

    Teams helper

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,6 @@ fn main() {
         position = mouse.get_position().expect("Unable to retrieve mouse position");
         mouse.move_to(position.x-1, position.y-1).expect("Unable to move mouse");
         thread::sleep(time::Duration::from_millis(5000));
+        mouse.press(&Keys::LEFT).expect("Unable to press button");
     }
 }
